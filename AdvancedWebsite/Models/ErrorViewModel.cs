61d85e5573e980740c406dd7a741cd0111b6cb08
Spencer Sterling <spencer.sterling2001@gmail.com>
2020-10-02 19:10:57 -0700

    Add project files.

---
+++ AdvancedWebsite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AdvancedWebsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
