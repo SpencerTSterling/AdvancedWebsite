46b657788c34c796a14baa400954b9f70f54c291
Spencer Sterling <63821532+SpencerTSterling@users.noreply.github.com>
2020-10-10 19:52:53 -0700

    Customize Identity pages (#13)
    * Scoffolded all ASP.NET Identity pages
    
    * Created the website database
    
    * Changed required account and modified password requirements
    
    * Set up lockout options
    
    * Modified Register page to allow username and email seperately
    
    * Changed Login page to require username instead of email
    
    * Modified login page to use username instead of email
    
    * Removed exernal login prompt from Login page

---
+++ AdvancedWebsite/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using AdvancedWebsite.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AdvancedWebsite.Areas.Identity.IdentityHostingStartup))]
+namespace AdvancedWebsite.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
