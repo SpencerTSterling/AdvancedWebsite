2edfb097c60c526788dae30d0c48d8a6965a06e7
Spencer Sterling <spencer.sterling2001@gmail.com>
2020-10-10 22:56:03 -0700

    Add migration for video lessons

--- AdvancedWebsite/Data/Migrations/20201011055524_AddVideoLesson.Designer.cs
+++ AdvancedWebsite/Data/Migrations/20201011055524_AddVideoLesson.Designer.cs
@@ -4,14 +4,16 @@ using AdvancedWebsite.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AdvancedWebsite.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201011055524_AddVideoLesson")]
+    partial class AddVideoLesson
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
