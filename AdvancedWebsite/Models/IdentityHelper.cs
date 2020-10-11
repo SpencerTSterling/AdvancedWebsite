d9f9a100e53921d6ee88cf37b2925231d2099e9d
Spencer Sterling <spencer.sterling2001@gmail.com>
2020-10-10 18:25:22 -0700

    Changed required account and modified password requirements

---
+++ AdvancedWebsite/Models/IdentityHelper.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdvancedWebsite.Models
+{
+    public static class IdentityHelper
+    {
+        /// <summary>
+        /// Sets the IdentityOptions 
+        /// </summary>
+        /// <param name="options"></param>
+        public static void SetIdentityOptions(IdentityOptions options)
+        {
+            // sign in options
+            options.SignIn.RequireConfirmedEmail = false;
+            options.SignIn.RequireConfirmedPhoneNumber = false;
+
+            // password options
+            options.Password.RequireDigit = false;
+            options.Password.RequireLowercase = false;
+            options.Password.RequireUppercase = false;
+            options.Password.RequiredLength = 8;
+            options.Password.RequireNonAlphanumeric = false;
+        }
+
+
+    }
+}
--- AdvancedWebsite/Startup.cs
+++ AdvancedWebsite/Startup.cs
@@ -12,6 +12,7 @@ using AdvancedWebsite.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using AdvancedWebsite.Models;
 
 namespace AdvancedWebsite
 {
@@ -30,11 +31,13 @@ namespace AdvancedWebsite
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<IdentityUser>(IdentityHelper.SetIdentityOptions)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
+
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
